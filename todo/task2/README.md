df37b65c1984745447d35ae5e7ed44053d7fdefe
dingchaofa <571797089@qq.com>
2017-05-21 09:55:44 +0800

    u

--- todo/task2/README.md
+++
@@ -1,6 +0,0 @@
-###### vue.js practise
-###### 我这个自带时时刷新服务器
-###### 根据 package.json 和 webpack.package.js 自行配置
-> npm run webserver
-
-
--- todo/task2/src/app.js
+++
@@ -1,9 +0,0 @@
-import bar from './bar';
-console.log(bar)
-import Vue from 'vue';
-var qpp = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hell121o Vue!'
-  }
-}) 
